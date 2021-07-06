67a2c17efbcb7cb7b2c9b0e699fcf2cafb2b2a73
wngr <oliver@wngr.de>
2021-07-06 16:11:13 +0200

    Add a quick intro

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
+//! # tracing-android
+//!
+//! Composable tracing layer which logs to logcat using the [Android NDK]'s
+//! __android_log_write function. The provided tag will be capped at 23 bytes.
+//! Logging events resulting in messages longer than 4000 bytes will result in
+//! multiple log lines in logcat. This avoids running into logcat's truncation
+//! behaviour.
+//!
+//! [Android NDK]: https://developer.android.com/ndk/reference/group/logging#__android_log_write
 mod android;
 mod layer;
 
-pub fn layer(name: &str) -> std::io::Result<layer::Layer> {
-    layer::Layer::new(name)
+/// Constructs a [`layer::Layer`] with the given `tag`.
+/// ```no_run
+/// // add the layer to an existing subscriber
+/// let subscriber = {
+///     use tracing_subscriber::layer::SubscriberExt;
+///     subscriber.with(tracing_win_event_log::layer("com.example").unwrap())
+/// }
+// // .. install the subscriber ..
+/// ```
+pub fn layer(tag: &str) -> std::io::Result<layer::Layer> {
+    layer::Layer::new(tag)
 }
